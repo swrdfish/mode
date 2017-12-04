85d486517cad7680227c1489e18e563e5836f4ee
Binayak Ghosh <ghoshbinayak@gmail.com>
2017-12-04 17:28:42 +0530

    remove send button, instead add attach button

--- app/components/InputArea.js
+++ app/components/InputArea.js
@@ -6,8 +6,7 @@ class InputArea extends React.Component {
 		return (
 			<div className="input-area-wrapper">
 				<input type="text" placeholder="Type message" className="input-area-inputfiled" />
-				<i class="fa fa-paperclip" id="attach-button" aria-hidden="true"></i>
-				<i className="fa fa-paper-plane" id="send-button" aria-hidden="true"></i>
+				<i className="fa fa-paperclip" id="attach-button" aria-hidden="true"></i>
 			</div>
 		)
 	}
