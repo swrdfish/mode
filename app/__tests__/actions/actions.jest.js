0af0ac9bcf9cc1512b9b7ddd9fc1cd8de2d2821c
Binayak Ghosh <ghoshbinayak@gmail.com>
2018-07-05 23:53:14 +0530

    include more tests

--- app/__tests__/actions/actions.jest.js
+++ app/__tests__/actions/actions.jest.js
@@ -1,163 +1,165 @@
 import * as Actions from '../../actions'
 
+describe('actions', () => {
+    it('Send correct add message signal', () => {
+        let uid = '20001'
+        let msgType = 'text'
+        let body = 'hello'
+        let isMine = false
+        let result = {
+          type: 'ADD_MSG',
+          uid,
+          msgType,
+          body,
+          isMine
+        }
+        expect(Actions.addMessage(uid, msgType, body, isMine)).toEqual(result)
+    })
 
-test('Send correct add message signal', () => {
-    let uid = '20001'
-    let msgType = 'text'
-    let body = 'hello'
-    let isMine = false
-    let result = {
-      type: 'ADD_MSG',
-      uid,
-      msgType,
-      body,
-      isMine
-    }
-    expect(Actions.addMessage(uid, msgType, body, isMine)).toEqual(result)
+
+    it('Send correct delete message signal', () => {
+      let id = 20001
+      let result = {
+        type: 'DEL_MSG',
+        id
+      }
+      expect(Actions.deleteMessage(id)).toEqual(result)
+    })
+
+
+    it('Send correct setContactFilter signal', () => {
+        let username = 'timon'
+        let result = {
+          type: 'SET_CONTACT_FILTER',
+          username
+        }
+        expect(Actions.setContactFilter(username)).toEqual(result)
+    })
+
+
+    it('Send correct addUser signal', () => {
+      let userData = {
+        uuid: 2051,
+        name: "timon"
+      }
+      let result = {
+        type: 'ADD_USER',
+        userData
+      }
+      expect(Actions.addUser(userData)).toEqual(result)
+    })
+
+
+    it('Send correct removeUser signal', () => {
+      let userData = {
+        uuid: 2051,
+        name: "timon"
+      }
+      let connectionManager = {some: 'value'}
+      let result = {
+        type: 'DEL_USER',
+        userData,
+        connectionManager
+      }
+      expect(Actions.removeUser(userData, connectionManager)).toEqual(result)
+    })
+
+
+    it('Send correct changeUser signal', () => {
+      let userData = {
+        uuid: 2051,
+        name: "timon"
+      }
+      let result = {
+        type: 'CHG_USER',
+        userData
+      }
+      expect(Actions.changeUser(userData)).toEqual(result)
+    })
+
+
+    it('Send correct login signal', () => {
+      let ref = {some: 'value'}
+      let username = 'timon'
+      let uid = 2211
+      let localIP = '192.168.0.1'
+      let result = {
+        type: 'LOGIN',
+        ref,
+        username, 
+        uid,
+        localIP
+      }
+      expect(Actions.login(ref, username, uid, localIP)).toEqual(result)
+    })
+
+
+    it('Send correct chat signal', () => {
+      let uid = 2211
+      let result = {
+        type: 'CHAT_USER',
+        uid
+      }
+      expect(Actions.chat(uid)).toEqual(result)
+    })
+
+
+    it('Send correct notify signal', () => {
+      let type = 'error'
+      let message = 'Code phat geya!'
+      let result = {
+        type: 'ADD_NOTIFICATION',
+        notificationType: type,
+        message
+      }
+      expect(Actions.notify(message, type)).toEqual(result)
+    })
+
+
+    it('Send correct unnotify signal', () => {
+      let index = 5
+      let result = {
+        type: 'DEL_NOTIFICATION',
+        index
+      }
+      expect(Actions.unnotify(index)).toEqual(result)
+    })
+
+
+    it('Send correct joinRoom signal', () => {
+      let roomRef = {some: 'value'}
+      let connectionManager = {some: 'othervalue'}
+      let result = {
+        type: 'JOIN_ROOM',
+        connectionManager,
+        roomRef
+      }
+      expect(Actions.joinRoom(connectionManager, roomRef)).toEqual(result)
+    })
+
+
+    it('Send correct setUsername signal', () => {
+      let username = 'timon'
+      let result = {
+        type: 'SET_USERNAME',
+        username
+      }
+      expect(Actions.setUsername(username)).toEqual(result)
+    })
+
+
+    it('Send correct addFiles signal', () => {
+      let id = 2048
+      let files = new Array(5)
+      let result = {
+        type: 'ADD_FILE',
+        id,
+        files
+      }
+      expect(Actions.addFiles(id, files)).toEqual(result)
+    })
 })
 
-
-test('Send correct delete message signal', () => {
-  let id = 20001
-  let result = {
-    type: 'DEL_MSG',
-    id
-  }
-  expect(Actions.deleteMessage(id)).toEqual(result)
-})
-
-
-test('Send correct setContactFilter signal', () => {
-    let username = 'timon'
-    let result = {
-      type: 'SET_CONTACT_FILTER',
-      username
-    }
-    expect(Actions.setContactFilter(username)).toEqual(result)
-})
-
-
-test('Send correct addUser signal', () => {
-  let userData = {
-    uuid: 2051,
-    name: "timon"
-  }
-  let result = {
-    type: 'ADD_USER',
-    userData
-  }
-  expect(Actions.addUser(userData)).toEqual(result)
-})
-
-
-test('Send correct removeUser signal', () => {
-  let userData = {
-    uuid: 2051,
-    name: "timon"
-  }
-  let connectionManager = {some: 'value'}
-  let result = {
-    type: 'DEL_USER',
-    userData,
-    connectionManager
-  }
-  expect(Actions.removeUser(userData, connectionManager)).toEqual(result)
-})
-
-
-test('Send correct changeUser signal', () => {
-  let userData = {
-    uuid: 2051,
-    name: "timon"
-  }
-  let result = {
-    type: 'CHG_USER',
-    userData
-  }
-  expect(Actions.changeUser(userData)).toEqual(result)
-})
-
-
-test('Send correct login signal', () => {
-  let ref = {some: 'value'}
-  let username = 'timon'
-  let uid = 2211
-  let localIP = '192.168.0.1'
-  let result = {
-    type: 'LOGIN',
-    ref,
-    username, 
-    uid,
-    localIP
-  }
-  expect(Actions.login(ref, username, uid, localIP)).toEqual(result)
-})
-
-
-test('Send correct chat signal', () => {
-  let uid = 2211
-  let result = {
-    type: 'CHAT_USER',
-    uid
-  }
-  expect(Actions.chat(uid)).toEqual(result)
-})
-
-
-test('Send correct notify signal', () => {
-  let type = 'error'
-  let message = 'Code phat geya!'
-  let result = {
-    type: 'ADD_NOTIFICATION',
-    notificationType: type,
-    message
-  }
-  expect(Actions.notify(message, type)).toEqual(result)
-})
-
-
-test('Send correct unnotify signal', () => {
-  let index = 5
-  let result = {
-    type: 'DEL_NOTIFICATION',
-    index
-  }
-  expect(Actions.unnotify(index)).toEqual(result)
-})
-
-
-test('Send correct joinRoom signal', () => {
-  let roomRef = {some: 'value'}
-  let connectionManager = {some: 'othervalue'}
-  let result = {
-    type: 'JOIN_ROOM',
-    connectionManager,
-    roomRef
-  }
-  expect(Actions.joinRoom(connectionManager, roomRef)).toEqual(result)
-})
-
-
-test('Send correct setUsername signal', () => {
-  let username = 'timon'
-  let result = {
-    type: 'SET_USERNAME',
-    username
-  }
-  expect(Actions.setUsername(username)).toEqual(result)
-})
-
-
-test('Send correct addFiles signal', () => {
-  let id = 2048
-  let files = new Array(5)
-  let result = {
-    type: 'ADD_FILE',
-    id,
-    files
-  }
-  expect(Actions.addFiles(id, files)).toEqual(result)
-})
 
 
 
--- app/__tests__/reducers/userList.jest.js
+++ app/__tests__/reducers/userList.jest.js
@@ -1,7 +1,131 @@
 import reducer from '../../reducers/userList'
+import ConnectionManager from '../../workers/connectionManager'
+
+
+jest.mock('../../workers/connectionManager');
 
 describe('userList reducer', () => {
     it('userList reducer should return initial state', () => {
         expect(reducer(undefined, {})).toEqual([])
+    })
+
+    it('should handle ADD_USER', () => {
+        let userData = {
+            foo: 'bar'
+        }
+
+        let action = {
+            type: 'ADD_USER',
+            userData
+        }
+
+        expect(reducer([], action)).toEqual([userData])
+
+        expect(reducer([userData], action)).toEqual([userData, userData])
+    })
+
+    it('should handle DEL_USER', () => {
+        let userData = {
+            uid: 1234,
+            foo: 'bar'
+        }
+
+        let connectionManager = new ConnectionManager()
+
+        let action = {
+            type: 'DEL_USER',
+            userData,
+            connectionManager
+        }
+
+        expect(reducer([userData], action)).toEqual([])
+        expect(connectionManager.closeConnection).toHaveBeenCalled()
+    })
+
+    it('should handle CHG_USER', () => {
+        let userData = {
+            uid: 1234,
+            foo: 'bar',
+            selected: true,
+            unseen: 5
+        }
+
+        let newUserData = {
+            uid: 1234,
+            foo: 'boo',
+            selected: true,
+            unseen: 6   
+        }
+
+        let action = {
+            type: 'CHG_USER',
+            userData: newUserData
+        }
+
+        expect(reducer([userData], action)).toEqual([{
+            uid: 1234,
+            foo: 'boo',
+            selected: true,
+            unseen: 5
+        }])
+    })
+
+    it('should handle ADD_MSG', () => {
+        let userData = {
+            uid: 1234,
+            foo: 'bar',
+            selected: true,
+            unseen: 5
+        }
+
+        let action = {
+            type: 'ADD_MSG',
+            uid: 1234
+        }
+
+        expect(reducer([userData], action)).toEqual([{
+            uid: 1234,
+            foo: 'bar',
+            selected: true,
+            unseen: 5
+        }])
+
+        userData.selected = false
+
+        expect(reducer([userData], action)).toEqual([{
+            uid: 1234,
+            foo: 'bar',
+            selected: false,
+            unseen: 6
+        }])
+
+        action.uid = 4567
+        expect(reducer([userData], action)).toEqual([{
+            uid: 1234,
+            foo: 'bar',
+            selected: false,
+            unseen: 5
+        }])
+    })
+
+    it('should handle CHAT_USER', () => {
+        let userData = {
+            uid: 1234,
+            foo: 'bar',
+            selected: true,
+            unseen: 5
+        }
+
+        let action = {
+            type: 'CHAT_USER',
+            uid: 1234
+        }
+
+        expect(reducer([userData], action)).toEqual([{
+            uid: 1234,
+            foo: 'bar',
+            selected: true,
+            unseen: undefined
+        }])
     })
 })
